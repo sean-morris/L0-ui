e648e77e8eb17ff18e0f923b0701ebb98dc60497
Sean Morris <sean.smorris@gmail.com>
2014-02-13 16:18:58 -0800

    Adding nodes via d3 and overlays

--- src/main/webapp/app/view/MainMapPanel.js
+++ src/main/webapp/app/view/MainMapPanel.js
@@ -52,11 +52,68 @@ Ext.define('CC.view.MainMapPanel', {
                 position: center
             }));
         }
-
+        //this.addLinkGoogle();
+        this.addNodeD3();
+        this.gmap.setCenter(new google.maps.LatLng(37.6203,-122.07620000000001));
         Ext.each(this.markers, this.addMarker, this);
         this.fireEvent('mapready', this, this.gmap);
     },
+    addLinkGoogle: function(){
+        var enc = google.maps.geometry.encoding
+        var o =  new google.maps.Polyline({
+                    path: enc.decodePath("{urdFf_bhVyEtCwA~@"),
+                    map: this.gmap,
+                    strokeColor: 'blue',
+                    icons: [{
+                      icon: { path: google.maps.SymbolPath.FORWARD_OPEN_ARROW },
+                      fillColor: 'blue',
+                      offset: '60%'
+                    }],
+                    strokeOpacity: 0.9,
+                    strokeWeight: 7,
+                  })
+        return o;
+    },
+    addNodeD3: function(){
+        var enc = google.maps.geometry.encoding
+        var overlay = new google.maps.OverlayView();
+        overlay.onAdd = function(){
+            var layer = d3.select(this.getPanes().overlayLayer).append("div")
+                          .attr("height", "100%")
+                          .attr("width", "100%")
+                          .attr("class", "map-elements")
+                overlay.draw = function(){
+                    var projection = this.getProjection();
+                    
+                    var marker = layer.selectAll("svg")
+                                  .data(enc.decodePath("m_sdF|ebhVaBdAkErC"))
+                                  .each(transform)
+                                  .enter().append("svg:svg")
+                                  .each(transform)
+                                  .attr("class", "marker")
+                                  .append("svg:circle")
+                                  .attr("cx", "10")
+                                  .attr("cy", "10")
+                                  .attr("r", "8")
+                                  .attr("fill", "white")
+                                  .attr("stroke", "blue")
+                                  .attr("stroke-width", "3")
+                                 
+                    function transform(d){
+                       d = new google.maps.LatLng(d.lat(), d.lng());
+                       d = projection.fromLatLngToDivPixel(d);
+                       return d3.select(this)
+                        .style("left", d.x + "px")
+                        .style("top", d.y + "px")
+                        
+                        
 
+                    }
+                };
+        };
+        overlay.setMap(this.gmap);
+  
+    },
     addMarker: function(marker) {
         marker = Ext.apply({
             map: this.gmap
